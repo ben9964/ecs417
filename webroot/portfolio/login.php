d0c42b13d69e4859f4efb6793f93fd54e2a6feab
ben9964 <35692789+ben9964@users.noreply.github.com>
2021-04-09 18:15:25 +0100

    Update login.php

--- webroot/portfolio/login.php
+++ webroot/portfolio/login.php
@@ -1,12 +1,6 @@
 <?php
-
-    $dbhost = getenv("MYSQL_SERVICE_HOST");
-    $dbport = getenv("MYSQL_SERVICE_PORT");
-    $dbuser = getenv("DATABASE_USER");
-    $dbpwd = getenv("DATABASE_PASSWORD");
-    $dbname = getenv("DATABASE_NAME");
     // Creates connection
-    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+    $conn = new mysqli(getenv("MYSQL_SERVICE_HOST"), "user", "password", "ecs417");
     // Checks connection
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
