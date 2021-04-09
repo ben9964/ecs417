b1ce1a07e9898fc0699c011428ec49115c93ec64
ben9964 <35692789+ben9964@users.noreply.github.com>
2021-04-09 18:02:58 +0100

    asda

--- webroot/portfolio/login.php
+++ webroot/portfolio/login.php
@@ -1,27 +1,25 @@
 <?php
-    console.log("hi");
 
-    if ($_SERVER["REQUEST_METHOD"] == "GET"){
-        $username = $_GET["email"];
-        $password = $_GET["pass"];
-
-
-        $dbhost = "127.0.0.1"
-        $dbport = "3306"
-        $dbuser = "root"
-        $dbpwd = ""
-        $dbname = "ecs417"
-        // Creates connection
-        $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-        // Checks connection
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
-
-        $sql = "SELECT * FROM USERS WHERE email = '$username' and password = '$password'";  
-        
-        console.log($conn->query($sql));
+    $dbhost = "127.0.0.1"
+    $dbport = "3306"
+    $dbuser = "root"
+    $dbpwd = ""
+    $dbname = "ecs417"
+    // Creates connection
+    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+    // Checks connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
     }
 
-
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+        $sql = "INSERT INTO USERS (email, password) VALUES ('terminated994@gmail.com', 'password1234')";
+        if ($conn->query($sql) === TRUE) {
+            //Your code
+        
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        $conn->close();
+    }
 ?>
