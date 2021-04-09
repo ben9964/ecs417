6e7ad7e939169e1cbde3eab3e8ba98455e4a70e8
ben9964 <35692789+ben9964@users.noreply.github.com>
2021-04-09 17:56:38 +0100

    aah

--- webroot/portfolio/login.php
+++ webroot/portfolio/login.php
@@ -6,11 +6,11 @@
         $password = $_GET["pass"];
 
 
-        $dbhost = getenv("MYSQL_SERVICE_HOST");
-        $dbport = getenv("MYSQL_SERVICE_PORT");
-        $dbuser = getenv("DATABASE_USER");
-        $dbpwd = getenv("DATABASE_PASSWORD");
-        $dbname = getenv("DATABASE_NAME");
+        $dbhost = "127.0.0.1"
+        $dbport = "3306"
+        $dbuser = "root"
+        $dbpwd = ""
+        $dbname = "ecs417"
         // Creates connection
         $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
         // Checks connection
