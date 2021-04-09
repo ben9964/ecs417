c3bb252cd9aa736eb8ac769636d73062b0a9e388
ben9964 <35692789+ben9964@users.noreply.github.com>
2021-04-09 18:27:55 +0100

    Update login.php

--- webroot/portfolio/login.php
+++ webroot/portfolio/login.php
@@ -1,19 +1,15 @@
 <?php
+    $dbhost = getenv("MYSQL_SERVICE_HOST");
+    $dbport = getenv("MYSQL_SERVICE_PORT");
+    $dbuser = getenv("DATABASE_USER");
+    $dbpwd = getenv("DATABASE_PASSWORD");
+    $dbname = getenv("DATABASE_NAME");
     // Creates connection
-    $conn = new mysqli(getenv("MYSQL_SERVICE_HOST"), "user", "password", "ecs417");
+    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
     // Checks connection
     if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+     die("Connection failed: " . $conn->connect_error);
     }
 
-    if ($_SERVER['REQUEST_METHOD'] == "POST"){
-        $sql = "INSERT INTO USERS (email, password) VALUES ('terminated994@gmail.com', 'password1234')";
-        if ($conn->query($sql) === TRUE) {
-            //Your code
-        
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        $conn->close();
-    }
+    
 ?>
