2c143b403739e95e2ec5ba2d6929777ad640a32b
ben9964 <35692789+ben9964@users.noreply.github.com>
2021-04-09 17:38:00 +0100

    test php
    php

---
+++ webroot/portfolio/login.php
@@ -0,0 +1,27 @@
+<?php
+    
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST["email"];
+        $password = $_POST["pass"];
+
+
+        $dbhost = getenv("MYSQL_SERVICE_HOST");
+        $dbport = getenv("MYSQL_SERVICE_PORT");
+        $dbuser = getenv("DATABASE_USER");
+        $dbpwd = getenv("DATABASE_PASSWORD");
+        $dbname = getenv("DATABASE_NAME");
+        // Creates connection
+        $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+        // Checks connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT * FROM USERS WHERE email = '$username' and password = '$password'";  
+        
+        print($conn->query($sql));
+    }
+
+
+?>
